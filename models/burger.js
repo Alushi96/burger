e5ad00ac23af3833ee0c0549950c55bbdf8a5bea
The Mechanic <jaber4132@gmail.com>
2020-05-03 17:53:08 -0400

    Step 4 - Added models directory and added burger.js file to directory

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(col, val, cb) {
+        orm.insertOne("burgers", col, val, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVal, condition, cb) {
+        orm.updateOne("burgers", objColVal, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
